fd789317aa54235484e1e91f68466f14a14a9f73
Igor Astakhov <astahov@gmail.com>
2022-09-05 00:15:07 -0400

    Adding t

--- routes/console.php
+++ routes/console.php
@@ -2,18 +2,26 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+Artisan::command('test', function () {
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    // $response = Http::asForm()->post( 'http://passport-app.test/oauth/token', [
+    //     // verizonapi
+    //     'grant_type' => 'client_credentials',
+    //     'client_id' => '97309a83-1b65-4a67-aed1-966ca7503011',
+    //     'client_secret' => 'jWFh6qO08iCwBctpXU5nz7Pjuh43HOGSYdK5yHQD',
+    //     'scope' => '',
+    // ]);
+    $o = json_decode( file_get_contents( base_path( 'storage/access_token' ) ) );
+    //
+    $accessToken = $o->access_token . 'x';
+    $response = Http::withHeaders([
+            'Accept' => 'application/json',
+                'Authorization' => 'Bearer '.$accessToken,
+    ])->get('http://passport-app.test/api/user');
+    // $i = file_put_contents( base_path( 'storage/access_token' ), $response->body() );
+
+    $this->comment( json_encode(  [  $response->status(), $response->body() ] ));
+    // return $response->json()['access_token'];
 })->purpose('Display an inspiring quote');
